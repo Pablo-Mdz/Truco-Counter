a90674338aaaa4695150e6969076c5bdd6e18031
Pablo <a@gmail.com>
2022-09-29 06:35:11 +0200

    BUTTON RESET

--- index.js
+++ index.js
@@ -1,44 +1,42 @@
-
+// increment Button
 let point1 = document.getElementById("sum-1")
-let point2 = document.getElementById("sum-2")
-
-let num1 = 0 
+let num1 = 0
 function increment1() {
     num1 += 1
     point1.textContent = num1
 }
 
+let point2 = document.getElementById("sum-2")
 let num2 = 0
 function increment2() {
     num2 += 1
     point2.textContent = num2
 }
-
+// delete button
 let correctionTeam1 = document.getElementById("sum-1")
-let correctionTeam2 = document.getElementById("sum-2")
-
 function delete1() {
     num1 -= 1
     correctionTeam1.textContent = num1
 }
+
+let correctionTeam2 = document.getElementById("sum-2")
 function delete2() {
     num2 -= 1
     correctionTeam2.textContent = num2
 }
+ 
+// reset Button
+let reset1Config1 = document.getElementById("sum-1")
+function reset1() {
+    num1 = 0
+    reset1Config1.textContent = num1
+}
+let reset1Config2 = document.getElementById("sum-2")
+function reset2() {
+    num2 = 0
+    reset1Config2.textContent = num2
+}
 
 
-
-
-// let countEl = document.getElementById("count-el")
-// let saveEl = document.getElementById("save-el")
-
-// console.log(countEl)
-
-// let count = 0
-
-// function increment() {
-//     count += 1
-//     countEl.textContent = count
-// }
 
 
